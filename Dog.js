3cbf7c322a9b9ecdf993c30272a9f69b7015582f
Aditya5021 <64189996+Aditya5021@users.noreply.github.com>
2021-02-15 10:11:40 +0530

    Add files via upload

---
+++ Dog.js
@@ -0,0 +1,61 @@
+var psw = document.getElementById("pwd");
+            var letter = document.getElementById("letter");
+            var capital = document.getElementById("capital");
+            var number = document.getElementById("number");
+            var length = document.getElementById("length");
+
+            // When the user clicks on the password field, show the message box
+            psw.onfocus = function() {
+              document.getElementById("message").style.display = "block";
+            }
+            
+            // When the user clicks outside of the password field, hide the message box
+            psw.onblur = function() {
+              document.getElementById("message").style.display = "none";
+            }
+
+            // When the user starts to type something inside the password field
+            psw.onkeyup = function() {
+              // Validate lowercase letters
+              var lowerCaseLetters = /[a-z]/g;
+              if(psw.value.match(lowerCaseLetters)) { 
+                letter.classList.remove("invalid");
+                letter.classList.add("valid");
+              } else {
+                letter.classList.remove("valid");
+                letter.classList.add("invalid");
+            }
+            
+              // Validate capital letters
+              var upperCaseLetters = /[A-Z]/g;
+              if(psw.value.match(upperCaseLetters)) { 
+                capital.classList.remove("invalid");
+                capital.classList.add("valid");
+              } else {
+                capital.classList.remove("valid");
+                capital.classList.add("invalid");
+              }
+            
+              // Validate numbers
+              var numbers = /[0-9]/g;
+              if(psw.value.match(numbers)) { 
+                number.classList.remove("invalid");
+                number.classList.add("valid");
+              } else {
+                number.classList.remove("valid");
+                number.classList.add("invalid");
+              }
+            
+              // Validate length
+              if(psw.value.length >= 6) {
+                length.classList.remove("invalid");
+                length.classList.add("valid");
+              } else {
+                length.classList.remove("valid");
+                length.classList.add("invalid");
+              }
+            }
+            
+            function Warn(){
+                alert("Your details have been saved.");
+            }
